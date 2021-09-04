77b71188e18ec445df73193fa2e6d3dbca014929
ajith <56103948+ajith-io@users.noreply.github.com>
2021-09-04 23:16:30 +0530

    main

--- 3_Implementation/main.cpp
+++ 3_Implementation/main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <string>
 #include "mypassword.h"
-<<<<<<< HEAD
 #include "remind.h"
-=======
-
->>>>>>> 663f3d939c7c2fa8fd816dc48ae25afc22311d43
 int main()
 {
     Userlogin app;
     std::cout<<"      Hello Alex\n";
     app.login();
     std::cin.get();
-<<<<<<< HEAD
     start();
-=======
->>>>>>> 663f3d939c7c2fa8fd816dc48ae25afc22311d43
 }
